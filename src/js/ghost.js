b5032f3030ad5bdd7b0a31f6100c4d588f98a478
NealRame <contact@nealrame.com>
2015-09-26 23:41:11 +0200

    Animate ghost body.

--- src/js/ghost.js
+++ src/js/ghost.js
@@ -7,19 +7,44 @@ const Vector2D = require('./vector2d');
 const GHOST_EATABLE_TIMEOUT = 8000;
 const GHOST_EATABLE_COLOR_1 = '#0000bb';
 const GHOST_EATABLE_COLOR_2 = '#aaa';
-const GHOST_BODY = new Path2D(`
-    M 0 1
-    V .3333
-    C 0 .0833, .3333 0, .5 0
-    S 1 .0833, 1 .3333
-    V 1
-    L .8333 .7777
-    L .6666 1
-    L .5 .7777
-    L .3333 1
-    L .1666 .7777
-    L 0 1
-`);
+const GHOST_BODY = [
+    new Path2D(`
+        M 0 1
+        L 0 0.4375
+        C 0 0.21875 0.25 0 0.5 0
+        C 0.75 0 1 0.21875 1 0.4375
+        L 1 1
+        L 0.875 0.875
+        L 0.75 1
+        L 0.625 0.875
+        L 0.5 1
+        L 0.375 0.875
+        L 0.25 1
+        L 0.125 0.875
+        L 0 1
+    `),
+    new Path2D(`
+        M 0 1
+        L 0 0.4375
+        C 0 0.21875 0.25 0 0.5 0
+        C 0.75 0 1 0.21875 1 0.4375
+        L 1 1
+        L 0.875 0.875
+        L 0.875 1
+        L 0.75 0.875
+        L 0.75 1
+        L 0.625 0.875
+        L 0.625 1
+        L 0.5 0.875
+        L 0.375 1
+        L 0.375 0.875
+        L 0.25 1
+        L 0.25 0.875
+        L 0.125 1
+        L 0.125 0.875
+        L 0 1
+    `)
+];
 const OUTER_GHOST_EYE = new Path2D(`
     M .142 0
     A .142 .142 0 1 1 .142 -0.0001
@@ -29,6 +54,16 @@ const INNER_GHOST_EYE = new Path2D(`
     A .071 .071 0 1 1 .071 -0.0001
 `);
 
+function *ghost_body_path_generator() {
+    let i = 0, j = 0;
+    for(;; i = (i + 1)%6) {
+        if (i === 0) {
+            j = (j + 1)%GHOST_BODY.length;
+        }
+        yield GHOST_BODY[j];
+    }
+}
+
 class Ghost extends(MovingEntity) {
     constructor(name, color = '#222', pos = new Vector2D(), speed = 0., behavior = {}) {
         super(pos, speed);
@@ -36,6 +71,7 @@ class Ghost extends(MovingEntity) {
         let _color = color;
         let _eatable = false;
         let _task_id = null;
+        let _body_path = ghost_body_path_generator();
         /* eslint-enable no-underscore-dangle */
         Object.defineProperty(this, 'name', {
           enumerable: true,
@@ -48,6 +84,10 @@ class Ghost extends(MovingEntity) {
         Object.defineProperty(this, 'points', {
             enumerable: true,
             get: () => 200
+        });
+        Object.defineProperty(this, 'bodyPath', {
+            enumerable: true,
+            get: () => _body_path.next().value
         });
         Object.defineProperty(this, 'eatable', {
             enumerable: true,
@@ -112,7 +152,7 @@ class Ghost extends(MovingEntity) {
             graphics.setBrush({
                 color: this.color
             });
-            graphics.fillPath(GHOST_BODY);
+            graphics.fillPath(this.bodyPath);
         }
 
         graphics.translate(new Vector2D([9/32, 13/32]));
