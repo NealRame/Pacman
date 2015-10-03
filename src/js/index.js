e31120631e83533d6652edc09117325ed1ab7145
NealRame <contact@nealrame.com>
2015-10-04 00:28:02 +0200

    Start game only when document is ready.

--- src/js/index.js
+++ src/js/index.js
@@ -5,73 +5,69 @@ import audio from './audio';
 import ui, {SCALE, TRANSLATE} from './ui';
 import Game from './game';
 
-const CANVAS_SIZE = graphics.size();
-const SCALE = 16;
-const game = new Game();
+function document_ready() {
+    if (document.readyState === 'complete') {
+        return Promise.resolve();
+    }
+    return new Promise((resolve) => {
+        document.onreadystatechange = () => {
+            if (document.readyState === 'complete') {
+                resolve();
+            }
+        };
+    });
+}
 
-const init_once = once(function() {
-    game.on('score-changed', function(score) {
-        ui.score = score;
-    });
-    game.on('high-score-changed', function(score) {
-        ui.highScore = score;
-    });
-    game.on('level-up', function(level) {
-        ui.level = level;
-    });
-    game.on('life-count-changed', function(life_count) {
-        ui.lifes = life_count;
-    });
-    game.on('game-over', function() {
+function init_game(game) {
+    game.on('score-changed', score => ui.score = score);
+    game.on('high-score-changed', score => ui.highScore = score);
+    game.on('level-up', level => ui.level = level);
+    game.on('life-count-changed', life_count => ui.lifes = life_count);
+    game.on('game-over', () => {
         if (game.lifes < 0) {
             game.reset();
         } else {
             game.levelUp();
         }
     });
-    game.on('game-started', function() {
-        ui.hideMessage();
-    });
-    game.on('reset', function() {
+    game.on('game-started', () => ui.hideMessage());
+    game.on('reset', () => {
         ui.highScore = game.highScore;
         ui.level = game.level;
         ui.lifes = game.lifes;
         ui.score = game.score;
         ui.showMessage('Ready');
     });
+    ui.on('direction-changed', direction => game.engine.updateDirection(direction));
+    ui.on('toggle-pause', () => game.togglePause());
+    ui.on('toggle-sound-mute', () => audio.toggle());
     game.reset();
-    ui.on('direction-changed', (direction) => {
-        game.engine.updateDirection(direction);
-    });
-    ui.on('toggle-pause', () => {
-        game.togglePause();
-    });
-    ui.on('toggle-sound-mute', () => {
-        audio.toggle();
-    });
-});
+}
 
 function draw(entity) {
     entity.draw(SCALE);
 }
 
-function run(timestamp) {
-    scheduler.update(timestamp);
-    init_once();
-    game.run();
-    graphics.clear();
-    draw(game.maze);
-    if (!game.paused) {
-        for (let drawable of [...game.resources, ...game.ghosts, game.pacman]) {
-            draw(drawable);
-        }
-    }
-    window.requestAnimationFrame(run);
-}
+document_ready()
+    .then(audio.initialize())
+    .then(() => {
+        const game = new Game();
+        const init = once(init_game);
 
-audio.initialize().then(() => {
+        function run(timestamp) {
+            scheduler.update(timestamp);
+            init(game);
+            game.run();
+            graphics.clear();
+            draw(game.maze);
+            if (!game.paused) {
+                for (let drawable of [...game.resources, ...game.ghosts, game.pacman]) {
+                    draw(drawable);
+                }
+            }
+            window.requestAnimationFrame(run);
+        }
+
         graphics.translate(TRANSLATE);
         window.requestAnimationFrame(run);
     });
-    window.requestAnimationFrame(run);
-});
