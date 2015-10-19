b6c0868ae2f073869e36d0ad5a180f67ded52640
NealRame <contact@nealrame.com>
2015-10-19 12:02:14 +0200

    Prod tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
-// var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 var watchify = require('watchify');
 
 function bundle_js(bundler) {
@@ -21,8 +21,20 @@ function bundle_js(bundler) {
         .pipe(buffer())
         .pipe(gulp.dest('.'))
         .pipe(sourcemaps.init({loadMaps: true}))
-            // .pipe(uglify())
         .pipe(sourcemaps.write())
+        .pipe(gulp.dest('.'));
+}
+
+function bundle_js_prod(bundler) {
+    return bundler.bundle()
+        .on('error', (function(err) {
+            gutil.log(err.message);
+            err.stream.end();
+        }).bind())
+        .pipe(source('public/assets/scripts/app.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('.'))
+        .pipe(uglify())
         .pipe(gulp.dest('.'));
 }
 
@@ -33,12 +45,23 @@ gulp.task('style', function() {
         .pipe(gulp.dest('./public/assets/css'));
 });
 
+gulp.task('style-prod', function() {
+    return gulp.src('./src/sass/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({extname: '.css'}))
+        .pipe(gulp.dest('./public/assets/css'));
+});
+
 gulp.task('watch-style', function() {
     return gulp.watch('./src/sass/*.scss', ['style']);
 });
 
 gulp.task('js', function() {
     return bundle_js(browserify('./src/js/index.js', {debug: true}).transform(babelify, {}));
+});
+
+gulp.task('js-prod', function() {
+    return bundle_js_prod(browserify('./src/js/index.js', {debug: false}).transform(babelify, {}));
 });
 
 gulp.task('watch-js', function() {
@@ -60,3 +83,4 @@ gulp.task('watch-js', function() {
 
 gulp.task('watch', ['watch-style', 'watch-js']);
 gulp.task('default', ['style', 'js']);
+gulp.task('prod', ['style-prod', 'js-prod']);
